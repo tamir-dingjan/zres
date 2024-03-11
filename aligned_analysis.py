881d142466a328ca897f8b7084be51a2cb7ab58c
Tamir Dingjan <tamir.dingjan@gmail.com>
2024-03-11 16:38:14 +0200

    Add ability to use a sequence alignment for structure cropped analysis

---
+++ aligned_analysis.py
@@ -0,0 +1,108 @@
+import logging
+import zres
+import os
+import sys
+import argparse
+import MDAnalysis as mda
+import pandas as pd
+
+
+logging.basicConfig(
+    filename="log-aligned_analysis",
+    filemode='w',
+    level=logging.DEBUG
+)
+
+# Argument parsing
+ap = argparse.ArgumentParser()
+
+ap.add_argument("-a", "--alignment", required=True, help="FASTA-formatted alignment file")
+ap.add_argument("-s", "--select", required=True, help="residue position in the alignment to begin analysis from")
+
+args = ap.parse_args()
+logging.debug(vars(args))
+
+if not os.path.isfile(args.alignment):
+    sys.exit("Couldn't find alignment file: ", args.alignment)
+
+try:
+    select_from_residue = int(args.select)
+except:
+    sys.exit("Couldn't interpret selection as a residue number: ", args.select)
+
+
+# Load in alignment file and cropping position
+residue_threshold_lookup = {}
+with open(args.alignment, "r") as alignment:
+    id = ""
+    read_associated_sequence = False
+    for line in alignment.readlines():
+        if ">" in line:
+            if "|" in line:
+                read_associated_sequence = True
+                id=line.split("|")[1]
+            continue
+        else:
+            if not read_associated_sequence:
+                continue
+            # Get the residue number corresponding to the given slignment position
+            # AlphaFold structures begin from resid 1
+            res_count = 0
+            for seq_i, seq in enumerate(line):
+                if seq_i == select_from_residue:
+                    residue_threshold_lookup[id] = res_count
+                    break
+                if seq != "-":
+                    res_count += 1
+            read_associated_sequence = False
+                    
+
+# Collect structures and remove any not found on disk
+structures = [x for x in residue_threshold_lookup.keys()]
+
+logging.debug(f"Structures collected from the alignment: {structures}")
+
+for i in structures:
+    if not os.path.isfile(i + "out.pdb"):
+        structures.remove(i)
+
+logging.debug(f"Found in total {len(structures)} aligned structures.")
+
+valid_results = []
+
+# For each structure
+for i in structures:
+
+    # Load the structurefile
+    structfile = i + "out.pdb"
+
+    if not os.path.isfile(structfile):
+        logging.debug("Structure file not found: ", structfile)
+        continue
+
+    struct = mda.Universe(structfile)
+
+    # Get this structure's selection from which to begin the analysis
+
+    residue_threshold = residue_threshold_lookup[i]
+    
+    # Set the desired portion of the in-memory structure to analyse using the "segid" attribute using the alignment
+    selected_segment_label = "B"
+    selected_segment = struct.add_Segment(segid=selected_segment_label)
+    struct.residues[residue_threshold:].segments = selected_segment
+
+    # Analyse residue Z-positions
+    analysis = zres.Zres(structfile).run_on_modified_structure(struct, segid=selected_segment_label, buffer=0)
+    if not analysis is None:
+        analysis.index = [structfile]*len(analysis)
+        valid_results.append(analysis)
+
+if len(valid_results) != 0:
+    results = pd.concat(valid_results)
+
+    # Save out summary
+    results.to_csv('results.csv')
+    logging.info("Results saved to 'results.csv'.")
+else:
+    logging.info("No valid results from structure analysis.")
+
--- zres.py
+++ zres.py
@@ -57,3 +57,51 @@ class Zres:
 
         # report as a pandas dataframe
         return pd.DataFrame(dict([(k,pd.Series(v)) for k,v in self.zres.items()])).melt(var_name="residue").dropna() 
+    
+
+    def run_on_modified_structure(self, struct, segid:str="A", buffer:int = 10):
+        # This method allows us to run the Zres analysis on an already-loaded structure rather than opening from disc
+        try:
+            if len(struct.atoms) > 0:
+                logging.debug("Passed structure has {%s} atoms." % len(struct.atoms))
+            else:
+                logging.warning("Passed structure has no atoms!")
+                return
+        except AttributeError:
+            logging.warning("Passed structure is not correctly loaded!")
+            return
+
+        # identify transmembrane region
+        outer = struct.select_atoms("resname DUM and name O and around 5 protein")
+        inner = struct.select_atoms("resname DUM and name N and around 5 protein")
+
+        # Catch cases without both membranes defined in OPM
+        if len(outer) == 0 or len(inner) == 0:
+            logging.warning("Structure file missing membrane information: %s" % self.structfile)
+            return
+
+        outer_z = np.mean(struct.coord[outer.indices][:,2])
+        inner_z = np.mean(struct.coord[inner.indices][:,2])
+        zmax = np.max((outer_z, inner_z))
+        zmin = np.min((outer_z, inner_z))
+
+        # Add border region buffers of 10 Angstrom?
+        buffer_size = buffer
+        #self.outer_z = self.outer_z + buffer_size
+        #self.inner_z = self.inner_z - buffer_size
+
+        # Store Z-positions of C-alphas within the transmembrane region plus buffer
+        
+        tmregion = struct.select_atoms(f"protein and segid {segid} and name CA and prop z <= {zmax + buffer_size} and prop z >= {zmin - buffer_size}")
+
+        # tmregion = calphas[np.where(np.logical_and((self.struct.xyz[0, calphas, 2] > np.min((self.outer_z, self.inner_z))),
+        #                                            (self.struct.xyz[0, calphas, 2] < np.max((self.outer_z, self.inner_z)))))]
+        
+        # Shift Z positions relative to membrane center
+        z_relative_to_center = struct.coord[tmregion.indices][:,2] - ((zmax + zmin)/2)
+
+        for atom, z in zip(tmregion, z_relative_to_center):
+            self.zres[atom.resname].append(z)
+
+        # report as a pandas dataframe
+        return pd.DataFrame(dict([(k,pd.Series(v)) for k,v in self.zres.items()])).melt(var_name="residue").dropna() 
